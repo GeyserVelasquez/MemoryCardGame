7e5e3f6904f5dc9c59b68c426a1d59f06be22834
Geyser Velasquez <gvelasquez.9312@unimar.edu.ve>
2023-10-15 14:57:13 -0400

    Tercer Commit: Se segmento el codigo en funciones para una mejor organizacion y visualizacion. Se optimizaron varios metodos del codigo y se anadieron nuevas clases.

--- src/memorycardgame/MemoryCardGame.java
+++ src/memorycardgame/MemoryCardGame.java
@@ -2,80 +2,126 @@ package memorycardgame;
 
 import java.util.Scanner;
 
-public class MemoryCardGame {
+public class MemoryCardGame { //Clase Principal. Contiene dos subclases: Jugador y Partida.
 
-    public static class Juego
-    {
-        public static int turnos = 0;
-        public static String Pturn = "P1";
-        
-        public void contar(){
-            if (turnos%2==0)
-            {
-                Pturn = "P1";
-            } else
-            {
-                Pturn = "P2";
-            }
-        }
-        
-        public void jugar()
+    public static class Jugador  //Esta clase es unicamente para llevar el puntaje. Literalmente es mas facil usar dos variables x,y para almacenar el puntaje. No se si exista una manera optima de hacerlo.
         {
-            Scanner read = new Scanner (System.in);
-            int i,j,holick1,holick2;
-            boolean isYourTurn = false;
-        
-            int[][]conjunto = 
-            {
-                {1,2,1,2},
-                {2,1,2,1},
-                {2,2,1,1}
-            };
-            
-            System.out.println("Escoge 1 elemento "+Pturn);
-            i = read.nextInt();
-            j = read.nextInt();
-            holick1 = conjunto [i][j];
-            System.out.println("Escoge otro elemento "+Pturn);
-            i = read.nextInt();
-            j = read.nextInt();        
-            holick2 = conjunto [i][j];
-        
-            if (holick1 == holick2)
-            {
-                isYourTurn = true;
-            } else {
-                ++turnos;
-            }
-        
-            contar();
-            
-            if (isYourTurn == true) // El codigo de este if podriamos meterlo dentro del de arriba si no usasemos el booleano para otra cosa.
-            {
-               System.out.println("Acertaste "+Pturn+", Juega Otra Vez");
-               jugar();
-            } else {
-               System.out.println("Fallaste, Juega "+Pturn);
-               jugar();
-            }
-        }
-        
-        public class Jugador extends Juego
-        {
-            public String name;
             public int score;
             
             public void Jugador(String name)
             {
-                this.name = name;
                 this.score = 0;
             }
+        }
+    
+    public static class Partida //En esta clase esta practicamente todo el codigo.
+    {
+        Scanner read = new Scanner (System.in);
+        Jugador P1 = new Jugador();
+        Jugador P2 = new Jugador();        
+        
+        public static int[][]conjunto = //Tenemos que ver como reflejamos esto en la interfaz.
+            {
+                {1,2,1,2},
+                {2,1,2,1},
+                {2,2,1,1},
+                {1,1,2,2}
+            };
+        
+        public static String Pturn = "P1"; // Pturn y turnos se inician de un modo que, el jugador 1 siempre empiece la partida.
+        public static int turnos = 0;
+        public static int i,j,k,l,holick1,holick2; // i y j son las coordenadas del primer elemento del arreglo, k y l son las del segundo. Se usan 4 variables para posteriormente saber cuales elementos se haran cero.
+        public static boolean isYourTurn = false; //Se inicia en False para correcto funcionamiento de los condicionales.
+        
+        public void mostrarMatriz()
+        {
+            for (i=0;i<=3;++i){
+                for (j=0;j<=3;++j){
+                    System.out.print(conjunto[i][j] + " ");
+                }
+                System.out.print("\n"); // Esto es un salto de linea para que las filas se muestren adecuadamente.
+            }
+        }
+        
+        public void realizarJugada() //Metodo para leer la jugada del jugador en curso
+        {
+            System.out.println("Escoge 1 elemento "+Pturn);
+            i = read.nextInt();
+            j = read.nextInt();
+            holick1 = conjunto [i][j]; //Obtencion del primer elemento.
+            System.out.println("Escoge otro elemento "+Pturn);
+            k = read.nextInt();
+            l = read.nextInt();        
+            holick2 = conjunto [k][l]; // Obtencion del segundo elemento.
+        }
+        
+        public void verificarJugada() // Metodo para verificar la jugada realizada con el metodo anterior.
+        {
+            while (holick1 == 0 || holick2 == 0) // Bucle repetitivo para asegurarse que los elementos no fueron escogidos con aterioridad.
+            {
+                System.out.println("Esos elementos ya fueron escogidos. Escoge otros.");
+                realizarJugada(); // Se llama de forma recursiva al metodo anterior para leer una nueva jugada
+            }
+            if (holick1 == holick2) // Condicional para verificar si la jugada fue correcta
+            {
+                isYourTurn = true; // Si es correcta, sigue siendo el turno del jugador en curso. Y los elementos que escogio se hacen cero.
+                conjunto [i][j] = 0;
+                conjunto [k][l] = 0;
+            } else 
+            {
+                ++turnos; // Si la jugada es incorrecta se le suma 1 al contador de turnos y se pasa al siguiente jugador
+                isYourTurn = false;
+            }
+        }
+        
+        public void verificarTurno(){ //Esta funcion no me gusta, y quisiera optimizarla
+            if (turnos%2==0)
+            {
+                Pturn = "P1";
+                if (isYourTurn == true)
+                {
+                    ++P1.score;
+                    System.out.println("Acertaste P1. Tu puntaje es: "+ P1.score +". Juega otra vez");
+                    jugar();
+                }else
+                {
+                    System.out.println("Fallaste, Juega P1");
+                    jugar();
+                }
+            }else
+            {
+                Pturn = "P2";
+                if (isYourTurn == true)
+                {
+                    ++P2.score;
+                    System.out.println("Acertaste P2. Tu puntaje es: "+ P2.score  +". Juega otra vez");
+                    jugar();
+                }else
+                {
+                    System.out.println("Fallaste, Juega P2");
+                    jugar();
+                }
+            }
+        }
+        
+        
+        public void jugar() //Metodo de la Clase Partida que recopila todos los metodos anteriores de forma ordenada.
+        {
+            mostrarMatriz();
+            
+            realizarJugada();
+         
+            verificarJugada();
+            
+            verificarTurno();
         }   
     }
+    
+    
     public static void main(String[] args) 
         {
-          Juego game = new Juego();
-          game.jugar();
+            Partida partida = new Partida(); // Se crea una instancia de la Clase Partida
+            partida.jugar(); // Para posteriormente ejecutar su metodo Jugar().
          }
     
 }
